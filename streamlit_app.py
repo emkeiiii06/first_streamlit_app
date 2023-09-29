8bb6ec362fa0d2ec36fcbe7cd8c9ce6cba9b2762
emkeiiii06 <146300560+emkeiiii06@users.noreply.github.com>
2023-09-29 09:56:32 +0800

    Update streamlit_app.py
    added normalize function in line 31

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,5 +25,9 @@ streamlit.header ("Fruityvice Fruit Advice!")
 
 import requests 
 fruity_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruity_response.json())
+streamlit.text(fruity_response.json()) # display the json data 
 
+# write your own comment -what does the next line do? 
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# write your own comment - what does this do?
+streamlit.dataframe(fruityvice_normalized)
