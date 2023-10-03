994ed2800b3adb952a02e09c504e694b818eb1c9
emkeiiii06 <146300560+emkeiiii06@users.noreply.github.com>
2023-10-03 08:39:36 +0800

    Update streamlit_app.py
    added additional fruit option line 47-49

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,5 +45,8 @@ streamlit.text("The fruit load list contains:")
 streamlit.dataframe(my_data_row)
 
 #allow user to enter new fruit 
+add_my_fruit = streamlit.text_input('What fruit would you like to add?','Kiwi')
+streamlit.write('The user entered ', add_my_fruit)
+
 
 
